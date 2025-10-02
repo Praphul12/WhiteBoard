e1d58c43f0e3df61a0a947a36b42da97a454d6c5
Praphul <praphul001@gmail.com>
2025-10-02 14:41:55 +0530

    minor bug

--- src/store/board-provider.js
+++ src/store/board-provider.js
@@ -133,7 +133,7 @@ const BoardProvider = ({ children }) => {
 
     async function fetchCanvas() {
       try {
-        const res = await fetch(`https://collaboard-backend-4zw3.onrender.com/${id}`, {
+        const res = await fetch(`https://collaboard-backend-4zw3.onrender.com/cnavas/${id}`, {
           headers: { Authorization: `Bearer ${token}` },
         });
         const data = await res.json();
